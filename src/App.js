43964cc331775388c3ce83cb453a3e2954d888eb
ketanthakkar <ketanthakkar.156@gmail.com>
2018-10-25 16:34:51 +0200

    Export to csv

--- src/App.js
+++ src/App.js
@@ -14,12 +14,17 @@ class App extends Component {
     this.handleClick = this.handleClick.bind(this);
   }
 
+  //Add row and column click handler
   handleClick() {
     let numberOfColumn = 0;
     let numberOfRow = 0;
 
     numberOfRow = this.refs.row.value;
     numberOfColumn = this.refs.column.value;
+
+    if(numberOfRow > 0 && numberOfColumn > 0) {
+      this.refs.exportBtn.disabled = false;
+    }
 
     var rowCount = Array.from({ length: numberOfRow }).map((_, rowIdx) => (
       <tr key={rowIdx}>{
@@ -28,7 +33,7 @@ class App extends Component {
         ))
       }
       <td>
-        <input  type="button" onClick={() => this.onDeleteEvent(rowIdx)} value="X" />
+        <input type="button" onClick={() => this.onDeleteEvent(rowIdx)} value="X" />
       </td>
       </tr>
       
@@ -41,6 +46,46 @@ class App extends Component {
     });
   }
 
+
+  ////Export CSV Click handler
+  onExportClick() {
+    var html = document.querySelector("table").outerHTML;
+	  this.export_to_csv(html, "table.csv");
+  }
+
+  //Export to CSV function
+  export_to_csv(html, filename) {
+    var csv = [];
+    var rows = document.querySelectorAll("table tr");
+    
+      for (var i = 0; i < rows.length-1; i++) {
+        var row = [], cols = rows[i].querySelectorAll("input");
+      
+          for (var j = 0; j < cols.length; j++) {
+              row.push(cols[j].value);
+          }
+          
+      csv.push(row.join(","));		
+    }
+    this.download_csv(csv.join("\n"), filename);
+  }
+
+  //Download CSV method
+  download_csv(csv, filename) {
+      var csvFile;
+      var downloadLink;
+
+      csvFile = new Blob([csv], {type: "text/csv"});
+
+      downloadLink = document.createElement("a");
+      downloadLink.download = filename;
+      downloadLink.href = window.URL.createObjectURL(csvFile);
+      downloadLink.style.display = "none";
+      document.body.appendChild(downloadLink);
+      downloadLink.click();
+  }
+
+  //Delete row handler
   onDeleteEvent = id => {
     let rowCount = this.state.rowCount;
     rowCount.splice(id, 1);
@@ -56,12 +101,15 @@ class App extends Component {
       <header className="App-header">
           <div className="userInput">
             <label>Row</label>
-            <input type="number" ref="row" />
+            <input type="text" ref="row" maxLength="2" onkeyup="this.value = this.value.replace(/[^a-z]/, '')" />
             <label>Column</label>
-            <input type="number" ref="column" />
+            <input type="text" ref="column" maxLength="2" onkeyup="this.value = this.value.replace(/[^a-z]/, '')" />
             <button onClick={this.handleClick}>Add</button>
+            <button ref="exportBtn" disabled onClick={() => this.onExportClick()}>Export to CSV</button>
           </div>
-          <Table rowCount={ this.state.rowCount } />
+          <div className="display">
+            <Table rowCount={ this.state.rowCount } />
+          </div>
         </header>
       </div>
     );
@@ -74,9 +122,7 @@ class Table extends Component {
       return (
         <div id="Table">
           <table>
-            <tbody>
               {rowCount}
-            </tbody>
           </table>
         </div>
       );
